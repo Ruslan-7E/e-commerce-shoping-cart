102e1051b68f15e5da9374d58477c487676d9602
Ruslan Mukhametshin <ruslanow597@gmail.com>
2023-07-02 16:48:42 +0300

    Initial commit

---
+++ src/test/java/pro/sky/ecommerceshopingcart/ECommerceShopingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.ecommerceshopingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceShopingCartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
